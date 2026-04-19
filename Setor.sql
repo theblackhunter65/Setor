727130e055d1e7b9baf7862e4a83f3aeccc4f171
theblackhunter65 <moraestiago1276@gmail.com>
2026-04-19 14:56:21 -0300

    Primeira versao: banco de dados Setor com tabelas funcionario e registros

---
+++ Setor.sql
@@ -0,0 +1,28 @@
+create database setor; 
+use setor; 
+ 
+create table funcionario( 
+ID int not null auto_increment, 
+nome varchar(100) not null, 
+primary key(ID), 
+salario int NOT NULL, 
+email varchar(222) not null, 
+senha varchar(150) not null, 
+cpf varchar(14) not null 
+); 
+ 
+create table registros( 
+id INT AUTO_INCREMENT PRIMARY KEY, 
+funcionario VARCHAR(100), 
+serial_number VARCHAR(100) UNIQUE, 
+data_hora DATETIME DEFAULT CURRENT_TIMESTAMP 
+); 
+ 
+INSERT INTO funcionario(nome, salario, email, senha, cpf) 
+VALUES('Jose', 1500, 'moraestiago1276@gmail.com', '123456', '123.456.789-01'); 
+ 
+INSERT INTO registros(funcionario, serial_number, data_hora) 
+VALUES('Jose', '1500', '2026-04-04 15:09:00'); 
+ 
+select * from funcionario; 
+select * from registros; 
